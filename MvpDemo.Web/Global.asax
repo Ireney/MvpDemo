457a04082e12c829ee58cf71325e690d072b28d9
ireney <ireney.berezniak@gmail.com>
2017-01-30 11:25:58 -0700

    Add project files.

---
+++ MvpDemo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvpDemo.Web.Global" Language="C#" %>
---
+++ MvpDemo.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvpDemo.Web.Startup))]
+namespace MvpDemo.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
